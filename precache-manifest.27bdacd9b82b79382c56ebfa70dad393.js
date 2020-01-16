0e4848d8beb6006a34ac5107f8dcabce814e22aa
Valeriy Samoilov <valerasam1012@gmail.com>
2020-01-16 06:25:16 +0200

    Updates

---
+++ precache-manifest.27bdacd9b82b79382c56ebfa70dad393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d4b31dc51ef67b21962daaf4f6614f",
+    "url": "/react-moviedb-app/index.html"
+  },
+  {
+    "revision": "23fd19ea1bd3f79c7bd5",
+    "url": "/react-moviedb-app/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "f51dec7468834a6c4d82",
+    "url": "/react-moviedb-app/static/css/main.b26aac61.chunk.css"
+  },
+  {
+    "revision": "23fd19ea1bd3f79c7bd5",
+    "url": "/react-moviedb-app/static/js/2.5ebd93a5.chunk.js"
+  },
+  {
+    "revision": "d4503a70721fea146de807f4f151ad0d",
+    "url": "/react-moviedb-app/static/js/2.5ebd93a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f51dec7468834a6c4d82",
+    "url": "/react-moviedb-app/static/js/main.eb99cf4e.chunk.js"
+  },
+  {
+    "revision": "3735be6eb39498569120",
+    "url": "/react-moviedb-app/static/js/runtime-main.29e214da.js"
+  }
+]);
